25b694d79ba77e32e98959b6e6f21bcca7a126d0
timok <timokujansuu@gmail.com>
2024-10-02 23:26:16 +0200

    Fix book deletion bug, change book to class

--- javascript.js
+++ javascript.js
@@ -13,44 +13,41 @@ submitButton.addEventListener("click", createBook);
 cancelButton.addEventListener("click", () => dialog.close());
 
 //Book object
-
-function Book(title, author, published, pages, genre, read) {
-    this.title = title;
-    this.author = author;
-    this.published = published;
-    this.pages = pages;
-    this.genre = genre;
-    this.read = read;
-    const id = null;
-}
-
-function DeleteBook(buttonID) {
-    const allBooks = document.querySelectorAll(".book");
-    for (item of allBooks) {
-        if (Number(item.getAttribute("data-id")) === buttonID) {
-            item.remove();
-            myLibrary.splice(buttonID, 1);
-        }
+class Book {
+    constructor(title, author, published, pages, genre, read) {
+        this.title = title;
+        this.author = author;
+        this.published = published;
+        this.pages = pages;
+        this.genre = genre;
+        this.read = read;
+        const id = null;
     }
 }
 
-Book.prototype.toggleRead = function (buttonID, bookReadButton) {
-    if (this.id === buttonID) {
-        if (this.read === "Read") {
-            this.read = "Not Read";
-            bookReadButton.textContent = "Not Read";
-            bookReadButton.style.backgroundColor = "rgb(216, 60, 60)";
+function DeleteBook(buttonID) {
+    for (item of myLibrary) {
+        if (item.id === buttonID) {
+            myLibrary.splice(myLibrary.indexOf(item), 1);
         }
-        else {
-            this.read = "Read";
-            bookReadButton.textContent = "Read";
-            bookReadButton.style.backgroundColor = "rgb(56, 109, 255)";
-        }
+    }
+    displayBooks();
+}
+
+Book.prototype.toggleRead = function (bookReadButton) {
+    if (this.read === "Read") {
+        this.read = "Not Read";
+        bookReadButton.textContent = "Not Read";
+        bookReadButton.style.backgroundColor = "rgb(216, 60, 60)";
+    }
+    else {
+        this.read = "Read";
+        bookReadButton.textContent = "Read";
+        bookReadButton.style.backgroundColor = "rgb(56, 109, 255)";
     }
 }
 
 //Called when creating new book in modal
-
 function createBook(e) {
     const form = document.querySelector("form");
     const title = form.elements.title.value;
@@ -64,72 +61,63 @@ function createBook(e) {
 }
 
 //Adds book to array and calls function to update page visually
-
 function addBookToLibrary(title, author, published, pages, genre, read) {
     const newBook = new Book(title, author, published, pages, genre, read)
     myLibrary.push(newBook);
-    newBook.id = myLibrary.length - 1;
     displayBooks();
 }
 
 //Loops through all books on page, removes everything, creates DOM elements for all books in array
-
 function displayBooks() {
     let oldBooks = document.querySelectorAll(".book");
     for (const book of oldBooks) { book.remove(); };
 
-    for (const book of myLibrary) {
+    for (let i = 0; i < myLibrary.length; i++) {
         const bookItem = bookList.appendChild(document.createElement("div"));
         bookItem.classList.add("book");
-        bookItem.setAttribute("data-id", book.id);
+        myLibrary[i].id = i;
 
         const title = bookItem.appendChild(document.createElement("div"));
         title.classList.add("title");
         const bookTitle = title.appendChild(document.createElement("div"));
         bookTitle.classList.add("booktitle");
-        bookTitle.innerText = book.title;
+        bookTitle.innerText = myLibrary[i].title;
         const deletebutton = title.appendChild(document.createElement("button"));
         deletebutton.setAttribute("type", "button")
         deletebutton.classList.add("removebookbutton")
         deletebutton.textContent = "X";
         const bookAuthor = bookItem.appendChild(document.createElement("div"));
         bookAuthor.classList.add("bookauthor", "bookinfocell");
-        bookAuthor.textContent = `Author:  ${book.author}`;
+        bookAuthor.textContent = `Author:  ${myLibrary[i].author}`;
         const bookPublished = bookItem.appendChild(document.createElement("div"));
         bookPublished.classList.add("bookpublished", "bookinfocell");
-        bookPublished.textContent = `Published:  ${book.published}`;
+        bookPublished.textContent = `Published:  ${myLibrary[i].published}`;
         const bookPages = bookItem.appendChild(document.createElement("div"));
         bookPages.classList.add("bookpages", "bookinfocell");
-        bookPages.textContent = `Pages:  ${book.pages}`;
+        bookPages.textContent = `Pages:  ${myLibrary[i].pages}`;
         const bookGenre = bookItem.appendChild(document.createElement("div"));
         bookGenre.classList.add("bookgenre", "bookinfocell");
-        bookGenre.textContent = `Genre:  ${book.genre}`;
+        bookGenre.textContent = `Genre:  ${myLibrary[i].genre}`;
         const bookRead = bookItem.appendChild(document.createElement("div"));
         bookRead.classList.add("bookread", "bookinfocell");
         const bookReadButton = bookRead.appendChild(document.createElement("button"));
         bookReadButton.setAttribute("type", "button")
         bookReadButton.classList.add("readbutton");
-        bookReadButton.textContent = book.read;
+        bookReadButton.textContent = myLibrary[i].read;
 
         //Sets initial color of read button
-
-        if (book.read === "Read") {
+        if (myLibrary[i].read === "Read") {
             bookReadButton.style.backgroundColor = "rgb(56, 109, 255)";
         }
-        else if (book.read === "Not read") {
+        else if (myLibrary[i].read === "Not read") {
             bookReadButton.style.backgroundColor = "rgb(216, 60, 60)";
         }
 
-        //Assigns the button a unique ID, so that it can find a matching book ID
-        deletebutton.setAttribute("data-id", book.id);
-        const buttonID = Number(deletebutton.getAttribute("data-id"));
-
         //Sets functionality of read button
-        bookReadButton.addEventListener("click", function () { book.toggleRead(buttonID, bookReadButton) });
+        bookReadButton.addEventListener("click", function () { myLibrary[i].toggleRead(bookReadButton) });
 
         //Sets functionality of delete book button
-        deletebutton.addEventListener("click", function () { DeleteBook(buttonID) });
+        deletebutton.addEventListener("click", function () { DeleteBook(i) });
 
     };
-
 }
